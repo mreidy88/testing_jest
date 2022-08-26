07b9832bafdf1dcfab65e3d9fac74c3042ed4af9
Mike Reidy <mreidy88@gmail.com>
2022-08-26 15:06:58 -0400

    Hello World

--- src/components/__tests__/todo.test.js
+++ src/components/__tests__/todo.test.js
@@ -7,4 +7,5 @@ test('should render todo component', () => {
     render(<Todo />);
     const todoElement = screen.getByTestId('todo-1');
     expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('Hello World');
 })
