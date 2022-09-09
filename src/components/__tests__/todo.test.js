43a0e5daac6de19c7d7d176656604f791a55dd92
Mike Reidy <mreidy88@gmail.com>
2022-09-09 14:43:28 -0400

    More Jesting

--- src/components/__tests__/todo.test.js
+++ src/components/__tests__/todo.test.js
@@ -32,3 +32,13 @@ test('matches snapshot', () => {
     expect(tree).toMatchSnapshot();
  
 })
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    // expect(z).not.toBeDefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+
+})
